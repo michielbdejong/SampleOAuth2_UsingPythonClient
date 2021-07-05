477f472850f467e0da09bd850630ad559e299235
Michiel de Jong <michiel@unhosted.org>
2021-07-05 14:29:00 +0200

    collectstatic

--- SampleOAuth2_UsingPythonClient/settings.py
+++ SampleOAuth2_UsingPythonClient/settings.py
@@ -121,6 +121,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"), ]
+STATIC_ROOT = os.path.join(BASE_DIR, "collect")
 
 # OAauth2 config here
 CLIENT_ID = '<EnterHere>'
